fe0c0a7d00fd144b9b64172874f4fa80a8a5c6ae
Steve Klebanoff <steve.klebanoff@gmail.com>
2022-01-03 12:40:37 -0800

    first one

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+require("dotenv").config({ path: ".env.local" });
+import * as ethers from "ethers";
+
+const envStringOrThrow = (val: string | undefined, name: string) => {
+  if (!val) {
+    throw new Error(`Please specify ${name} in ENV`);
+  } else {
+    return val;
+  }
+};
+
+export const config = {
+  covalentApi: envStringOrThrow(
+    process.env.COVALENT_API_KEY,
+    "COVALENT_API_KEY"
+  ),
+  rpcUrl: envStringOrThrow(process.env.RPC_URL, "RPC_URL"),
+};
+
+export const ethersProvider = new ethers.providers.JsonRpcProvider(
+  config.rpcUrl
+);
